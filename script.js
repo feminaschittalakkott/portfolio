1820c0edd98b4b9affe15f801056fc040478e235
feminaschittalakkott <feminasc442@gmail.com>
2024-09-17 11:17:43 +0400

    Updates

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-$(function () {    
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop()) {
-          $("nav").addClass("black");
-        } else {
-          $("nav").removeClass("black");
-        }
-        });
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+      $("nav").addClass("black");
+    } else {
+      $("nav").removeClass("black");
+    }
+  });
 
-        $('.icon').on('click', function() {
-        $('.menu').toggleClass('showing');
-    });
+  $('.icon').on('click', function () {
+    $('.menu').toggleClass('showing');
+  });
+  $('.menu a').on('click', function () {
+    $('.menu').removeClass('showing');
+  });
 
 }());
